46dfdbef390e4b81f4fc8c093418df3db8e485a0
Crunkagator <briantheblack@gmail.com>
2018-09-17 17:59:02 +0300

    sample text

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  a = preferences;
+  let count = 0;
+  for (let i = 0; i<a.length; i++){
+      if ((a[i] -1) == i) continue
+        if((a[(a[i] -1)]-1) == (a[i] -1)) continue
+          if ((a[(a[(a[i] -1)]-1)]-1) == (a[(a[i] -1)]-1)) continue
+            if((a[(a[(a[i] -1)]-1)]-1) == i){
+              count++
+            }
+  }
+  return (count/3);
 };
